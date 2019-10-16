7f93ec95676eb47ae8271d4110ccf113346bc4f9
maratovnaa08 <aiym.eltaeva@gmail.com>
2019-10-16 19:29:22 +0600

    q2

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,11 +1,11 @@
 package main
 
-
 import "github.com/01-edu/z01"
 
-
+//func main do something
 func main() {
-for i:='a'; i<='z'; i++{
-    z01.PrintRune(i)}
-z01.PrintRune('\n')
+	for i := 'a'; i <= 'z'; i++ {
+		z01.PrintRune(i)
+	}
+	z01.PrintRune('\n')
 }
