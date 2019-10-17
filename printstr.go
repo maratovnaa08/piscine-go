979b4bb161ed0bfa41b0b0bda64b13a6c673cc6c
maratovnaa08 <aiym.eltaeva@gmail.com>
2019-10-17 20:33:53 +0600

    q3-5

--- printstr.go
+++ printstr.go
@@ -1,10 +1,10 @@
-package main
+package piscine
 
 import "github.com/01-edu/z01"
 
 //S PrinStr do something
 func PrintStr(str string) {
 	for _, b := range str {
-		z01.PrintRune(str)
+		z01.PrintRune(b)
 	}
 }
