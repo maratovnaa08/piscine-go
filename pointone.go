73fb9ce25b657c279f4bac8c5f7919e788c253e3
maratovnaa08 <aiym.eltaeva@gmail.com>
2019-10-17 17:18:20 +0600

    q3-1

---
+++ pointone.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+//PrintOne 123
+func PrintOne(n *int) {
+	var guess = new(int)
+	*guess = 12345
+	point(gues)
+}
+func point(n *int)
+*n = 1
+	z01.PrintRune(n)
+}
+z01.PrintRune('\n')
