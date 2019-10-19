e47b89132877f03ee7caf36d3df3619239c44830
maratovnaa08 <aiym.eltaeva@gmail.com>
2019-10-19 15:10:42 +0600

    r1a

---
+++ raid1a.go
@@ -0,0 +1,22 @@
+package piscine
+
+import "github.com/01-edu/z01.PrintRune"
+
+func Raid1a(x,y int) {
+	for i := 0; i < y; i++ ) { 
+		if ( i = 0 || i == y-1) {
+			for j := 0; j < x; j++ {
+				if (j == 1 || j = x-1) {
+					z01.PrintRune.print(48)
+				}	
+				else { PrintRune(45) 
+				}				
+			}
+		} else { for j := 0; j < x; j++ {
+					if (j ==1 || j = x-1) {
+						z01.PrintRune(42)
+					} else { z01.PrintRune(65) 
+					}
+		}	
+
+}
