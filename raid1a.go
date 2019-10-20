6397428945e710d4125a452a68a1523d0b147540
maratovnaa08 <aiym.eltaeva@gmail.com>
2019-10-20 13:20:16 +0600

    raid1a

--- raid1a.go
+++ raid1a.go
@@ -1,22 +1,26 @@
-package piscine
+package main //piscine
 
-import "github.com/01-edu/z01.PrintRune"
+import "github.com/01-edu/z01"
 
-func Raid1a(x,y int) {
-	for i := 0; i < y; i++ ) { 
-		if ( i = 0 || i == y-1) {
-			for j := 0; j < x; j++ {
-				if (j == 1 || j = x-1) {
-					z01.PrintRune.print(48)
-				}	
-				else { PrintRune(45) 
-				}				
+func Raid1a(x, y int) {
+	for i := 0; i < y; i++ {
+		for j := 0; j < x; j++ {
+			if (i == 0 && j == 0) || (i == 0 && j == x-1) {
+				z01.PrintRune(111)
+			} else if (i == y-1 && j == 0) || (i == y-1 && j == x-1) {
+				z01.PrintRune(111)
+			} else if (i > 0 && j == 0) || (i > 0 && j == x-1) {
+				z01.PrintRune(124)
+			} else if (i == 0 && j > 0 && j < x-1) || (i == y-1 && j > 0 && j < x-1) {
+				z01.PrintRune(45)
+			} else {
+				z01.PrintRune(' ')
 			}
-		} else { for j := 0; j < x; j++ {
-					if (j ==1 || j = x-1) {
-						z01.PrintRune(42)
-					} else { z01.PrintRune(65) 
-					}
-		}	
+		}
+		z01.PrintRune(rune(10))
+	}
+}
 
+func main() {
+	Raid1a(5, 3)
 }
