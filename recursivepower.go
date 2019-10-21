8bbf2f9a9b6e97b5636a2e4d78d4340b04ba22f1
maratovnaa08 <aiym.eltaeva@gmail.com>
2019-10-21 22:27:16 +0600

    quest4-4

---
+++ recursivepower.go
@@ -0,0 +1,16 @@
+package piscine
+
+func RecursivePower(nb int, power int) int {
+	if power < 0 {
+		return 0
+	}
+	if nb == 0 {
+		return 1
+	}
+	if nb == 1 {
+		return 1
+	} else {
+		return RecursiveFactorial(power-1) * nb
+	}
+	return 0
+}
