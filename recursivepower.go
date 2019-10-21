76a4dfcbe9c07afa9ad35b7deed2a69fbbe94c40
maratovnaa08 <aiym.eltaeva@gmail.com>
2019-10-21 22:34:08 +0600

    quest4-4

--- recursivepower.go
+++ recursivepower.go
@@ -4,10 +4,8 @@ func RecursivePower(nb int, power int) int {
 	if power < 0 {
 		return 0
 	}
-	if nb == 1 {
-		return 1
-	} else {
-		return RecursivePower(power-1) * nb
+	if power > 0 {
+		return nb * RecursivePower(nb, power-1)
 	}
 	return 0
 }
