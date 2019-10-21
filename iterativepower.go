06ba8c94ebe84ae40c410640937ba855e2e22cfe
maratovnaa08 <aiym.eltaeva@gmail.com>
2019-10-21 22:23:52 +0600

    quest4-3

--- iterativepower.go
+++ iterativepower.go
@@ -2,14 +2,11 @@ package piscine
 
 func IterativePower(nb int, power int) int {
 	if power < 0 {
-		x := 1
-		for i := 1; i <= power; i++ {
-			i = i * nb
-			return x
-		} else
-		power = 0 {
-			return 1
-		}
+		return 0
 	}
-	return 0
+	result := 1
+	for i := 1; i <= power; i++ {
+		result = result * nb
+	}
+	return result
 }
