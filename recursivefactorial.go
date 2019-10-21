100dedc40ed4767e9acceed17b8fb2da20041bd9
maratovnaa08 <aiym.eltaeva@gmail.com>
2019-10-21 21:32:17 +0600

    quest4-2

---
+++ recursivefactorial.go
@@ -0,0 +1,13 @@
+package piscine
+
+func RecursiveFactorial(nb int) int {
+	if nb < 0 || nb > 20000 {
+		return 0
+	}
+	if nb == 1 {
+		return 1
+	} else {
+		return RecursiveFactorial(nb-1) * nb
+	}
+	return 0
+}
