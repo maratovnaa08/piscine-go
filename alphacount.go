528c9ff2d349e932ebc198ee8d1754e341ba9b7c
maratovnaa08 <aiym.eltaeva@gmail.com>
2019-10-22 17:19:55 +0600

    q5-1

--- alphacount.go
+++ alphacount.go
@@ -5,11 +5,11 @@ func AlphaCount(str string) int {
 	for range str {
 		counter++
 	}
-	str_rune := []rune(str)
+	letter := []rune(str)
 	for i := 0; i < counter-1; i++ {
-		if (str_rune[i] >= 'a' || str_rune[i] <= 'z') && (str_rune[i] >= 'A' || str_rune[i] <= 'Z') {
-			str_rune++
+		if (letter[i] >= 'a' || letter[i] <= 'z') && (letter[i] >= 'A' || letter[i] <= 'Z') {
+			letter++
 		}
-		return (AlphaCount(str_rune))
+		return (AlphaCount(letter))
 	}
 }
