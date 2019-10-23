d4a8e2ac82a33670bfee69268919964e53888adc
maratovnaa08 <aiym.eltaeva@gmail.com>
2019-10-24 00:33:52 +0600

    q5-5

--- nrune.go
+++ nrune.go
@@ -1,13 +1,13 @@
 package piscine
 
 func NRune(s string, n int) rune {
-	str_rune := []rune(s)
+	s_rune := []rune(s)
 	count := 0
-	for range str_rune {
+	for range s_rune {
 		count = count + 1
 	}
 	if n <= count-1 {
-		return str_rune[n-1]
+		return s_rune[n-1]
 	}
 	return 0
 }
