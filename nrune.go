70e574494a1edfb5d075f3a7428b9dc4f475de03
maratovnaa08 <aiym.eltaeva@gmail.com>
2019-10-24 00:29:54 +0600

    q5-5

---
+++ nrune.go
@@ -0,0 +1,13 @@
+package piscine
+
+func NRune(s string, n int) rune {
+	str_rune := []rune(s)
+	count := 0
+	for range str_rune {
+		count = count + 1
+	}
+	if n <= count-1 {
+		return str_rune[n-1]
+	}
+	return str_rune[0]
+}
