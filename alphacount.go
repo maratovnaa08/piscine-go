1e1e59be4b0fe59ae2471e130b30dadd05aacebb
maratovnaa08 <aiym.eltaeva@gmail.com>
2019-10-23 22:34:07 +0600

    q5-11

--- alphacount.go
+++ alphacount.go
@@ -3,7 +3,7 @@ package piscine
 func AlphaCount(str string) int {
 	counter := 0
 	for _, letter := range str {
-		if (letter >= 65 || letter <= 90) && (letter >= 97 || letter <= 122) {
+		if (letter >= 65 && letter <= 90) || (letter >= 97 && letter <= 122) {
 			counter++
 		}
 	}
