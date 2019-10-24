3afbfe9e81c06b8a6eb47a886e5925a6185b12a5
maratovnaa08 <aiym.eltaeva@gmail.com>
2019-10-24 18:37:31 +0600

    q5-7

--- index.go
+++ index.go
@@ -1,16 +1,27 @@
-package piscine
+package piscine //main
 
+//import "fmt"
 func Index(s string, toFind string) int {
-	s_rune := []rune(s)
+	//	s_rune := []rune(s)
 	toFind_rune := []rune(toFind)
 	count_find := 0
 	for range toFind_rune {
 		count_find = count_find + 1
 	}
-	for index, str_s := range s_rune {
-		if count_find > 0 && str_s == toFind_rune[0] {
+	count_s := 0
+	for range s {
+		count_s++
+	}
+	for index := 0; index < count_s; index++ {
+		if count_find > 0 && s[index] == toFind[0] {
 			return index
 		}
 	}
 	return -1
 }
+
+//func main() {
+//	fmt.Println(Index("1Vk", ""))
+//	fmt.Println(Index("!9h'jc09EGsS9", "09tfRtkg5p3HH"))
+//	fmt.Println(Index("kg^(g$$h_MF>9", ">hgDLCyht&=AX"))
+//}
