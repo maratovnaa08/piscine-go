8554cd31508c3a958e9e5b82352932065bcfe4ba
maratovnaa08 <aiym.eltaeva@gmail.com>
2019-10-24 17:31:19 +0600

    quest57

--- index.go
+++ index.go
@@ -1,10 +1,12 @@
 package piscine
 
 func Index(s string, toFind string) int {
+
 	a := rune[s]
 	b := rune[toFind]
 	c := lent(a)
 	d := lent(b)
+
 	for i := 0; i < c-d; i++ {
 		if toFind == s[i:i+d] {
 			return (i)
