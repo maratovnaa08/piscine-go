a2b4fd1002b2df0df6d861c901cba876df32dde0
Aiymgul Yeltaeva <54427669+maratovnaa08@users.noreply.github.com>
2019-10-24 18:54:47 +0600

    Update index.go

--- index.go
+++ index.go
@@ -3,16 +3,17 @@ package piscine
 func Index(s string, toFind string) int {
 	//s_rune := []rune(s)
 	toFind_rune := []rune(toFind)
+	S_rune := []rune(s)
 	count_find := 0
 	for range toFind_rune {
 		count_find = count_find + 1
 	}
 	count_s := 0
-	for range s {
+	for range S_rune {
 		count_s++
 	}
 	for index := 0; index < count_s; index++ {
-		if count_find > 0 && string(s[index]) == string(toFind[0]) {
+		if count_find > 0 && string(S_rune[index]) == string(toFind_rune[0]) {
 			return index
 		}
 	}
