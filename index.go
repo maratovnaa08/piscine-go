601cae6989ff46b40c51b6f4a65ec5ddd7e7d97b
maratovnaa08 <aiym.eltaeva@gmail.com>
2019-10-24 17:55:45 +0600

    quest58

--- index.go
+++ index.go
@@ -2,14 +2,14 @@ package piscine
 
 func Index(s string, toFind string) int {
 	s_rune := []rune(s)
-	to_Find_rune := []rune(toFind)
+	toFind_rune := []rune(toFind)
 	count_find := 0
-	for range to_Find_rune {
+	for range toFind_rune {
 		count_find = count_find + 1
 	}
-	for Index, str_s := range s_rune {
-		if count_find > 0 && str_s == to_Find_rune[0] {
-			return Index
+	for index, str_s := range s_rune {
+		if count_find > 0 && str_s == toFind_rune[0] {
+			return index
 		}
 	}
 	return -1
